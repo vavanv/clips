14e7199daaecd7d1b47cc412bcae31963c5a0805
Vladimir V <vladimir.vorobiev@gmail.com>
2023-10-14 18:46:34 -0400

    add input component, tab, and tab container

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ModalComponent } from './modal/modal.component';
+import { TabContainerComponent } from './tab-container/tab-container.component';
+import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
 // import { ModalService } from '../services/modal.service';
 
 @NgModule({
-  declarations: [ModalComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent],
+  declarations: [
+    ModalComponent,
+    TabContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabComponent,
+    TabContainerComponent,
+    InputComponent,
+  ],
   // providers: [ModalService],
 })
 export class SharedModule {}
---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+  confirm_password = new FormControl('', [Validators.required]);
+  phoneNumber = new FormControl('', [Validators.required]);
+
+  registerForm: FormGroup = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber,
+  });
+}
