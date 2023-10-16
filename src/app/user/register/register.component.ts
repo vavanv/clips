52f68dc3cf0e4a47cdbac90eafec70c4a5643d2a
Vladimir V <vladimir.vorobiev@gmail.com>
2023-10-15 21:02:52 -0400

    add number and email validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -9,7 +9,11 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterComponent {
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.required, Validators.email]);
-  age = new FormControl('', [Validators.required]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120),
+  ]);
   password = new FormControl('', [Validators.required]);
   confirm_password = new FormControl('', [Validators.required]);
   phoneNumber = new FormControl('', [Validators.required]);
