7853b5a535d54b44b93f7b9fda0dedca67512929
Vladimir Vorobiev <vladimir.vorobiev@intelex.com>
2023-10-24 11:23:58 -0400

    add db

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +9,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+
+  inSubmission = false;
+
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.required, Validators.email]);
   age = new FormControl('', [
@@ -38,9 +44,36 @@ export class RegisterComponent {
     phoneNumber: this.phoneNumber,
   });
 
-  register() {
+  async register() {
     this.showAlert = true;
     this.alertMsg = 'Please wait! Your account is being created.';
     this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    const { email, password } = this.registerForm.value;
+    try {
+      const userCredentials = await this.auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await this.db.collection('users').add({
+        name: this.name.value,
+        email: this.email.value,
+        age: this.age.value,
+        phoneNumber: this.phoneNumber.value,
+        // uid: userCredentials.user?.uid,
+      });
+    } catch (error) {
+      console.error(error);
+
+      this.alertMsg = 'An unexpected error occurred. Please try again later';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+
+    this.alertMsg = 'Your account has been created successfully';
+    this.alertColor = 'green';
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB8y4U9_K05d66bAo4iZDelixMJCVs2spc',
+    authDomain: 'clips-ad6fe.firebaseapp.com',
+    projectId: 'clips-ad6fe',
+    storageBucket: 'clips-ad6fe.appspot.com',
+    messagingSenderId: '908471542915',
+    appId: '1:908471542915:web:4ce5df411732dc7f5f27b6',
+  },
+};
